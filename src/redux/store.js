c2902bface0aab2cb3af8b288d7aef4a456a03f6
amrutsabale <samrut355@gmail.com>
2021-03-20 21:51:20 +0530

    Added redux-logger middleware & devetools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
