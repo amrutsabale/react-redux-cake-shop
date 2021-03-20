9bf3bf888ed0d470a22609a879d8db1aaaf4270a
amrutsabale <samrut355@gmail.com>
2021-03-20 18:43:54 +0530

    Used state & dispatch in CakeContainer

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
+
+const CakeContainer = (props) => {
+  return (
+    <div>
+      <h1>No of Cakes: {props.numOfCakes}</h1>
+      <button onClick={props.buyCake}>Buy a Cake</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
